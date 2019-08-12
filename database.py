f4f14e6fc6a282c9777fc733783365d864c396fd
bcahlit <bcahlit@gmail.com>
2019-08-12 11:03:09 +0800

    完成了,, 剩下就debug了,,

--- database.py
+++ database.py
@@ -28,17 +28,3 @@ class DbHandler():
   def insert(self, Vkey, Vvalue, changStr):
     self.__c.execute('insert into Chromosome (Vkey, Vvalue, paramStr) values (\'{}\', \'{}\', \'{}\')'
           .format(Vkey,Vvalue,changStr))
-
-  def dict_player(self, cycle, object):
-    dict_return = {}
-    cursor = self.__c.execute('SELECT POSX,POSY,VELX,VELY,BODYDIR,STAMINA FROM RCDATA \
-                              WHERE OBJECT = ' + str(object) +
-                              ' AND CYCLE = ' + str(cycle))
-    for row in cursor:
-      dict_return['posx'] = row[0]
-      dict_return['posy'] = row[1]
-      dict_return['velx'] = row[2]
-      dict_return['vely'] = row[3]
-      dict_return['bodydir'] = row[4]
-      dict_return['stamina'] = row[5]
-    return dict_return
--- geneticMutationFrame.py
+++ geneticMutationFrame.py
@@ -5,10 +5,22 @@ import re
 import sqlite3
 import random
 import variation
+import operator
+import shutil
 from database import DbHandler
 
+# 适应度
+Adaptability = {}
 # 染色体数量
 chromosomeNum = 10
+# 自然选择概率
+selectionProbability = {}
+# 染色体复制的比例(每代中保留适应度较高的染色体直接成为下一代)
+cp = 0.3
+# 迭代次数
+iteratorNum = 10
+# 交叉的染色体数
+crossoverMutationNum = chromosomeNum - chromosomeNum * cp
 design_point = re.compile(r'[\s\S]*(\$[\s\S]*\$)[\s\S]*')
 # 用来将设计点的内容分隔开
 reSplitDesignPoint = re.compile(r'[\,\$]+')
@@ -34,9 +46,51 @@ def initOriginChromosome(files):
     f.write("0")
 
 def calAdaptability():
-  pass
+  global Adaptability
+  Adaptability.clear()
+  with open('genstate', 'r') as f:
+    genstate = f.read()
+  # 计算适应度
+  for i in range(0,chromosomeNum):
+    Adaptability['{}_{}.db'.format(int(genstate)+1,i)]=random.randint(30,60)
+
+  # sorted_x=sorted(x.items(), key=operator.itemgetter(1))
+  # print(Adaptability)
+  # for i in range(0,crossoverMutationNum):
+
+  # # 先随机生成，以后再做，，，
+  # for i in range(0,chromosomeNum):
+  #   print("chromosomeNum")
+  #   with open('./Adaptability/{}_{}_value'.format(int(genstate)+1), 'w') as f:
+  #     f.write(str(random.randint(30,60)))
+
+# 计算自然选择概率
+def calSelectionProbability():
+    global selectionProbability
+    global Adaptability
+    selectionProbability.clear()
+    # 计算适应度总和
+    sumAdaptability = 0.0
+    for eachvalue in Adaptability.itervalues():
+      sumAdaptability+=eachvalue
+    # 计算每条染色体的选择概率
+    for key, value in d.iteritems():
+        selectionProbability[key]= (value / sumAdaptability)
+
+def RWS():
+  global selectionProbability
+  sum = 0
+  rand = random.random()
+  for key,value in selectionProbability:
+        sum += value
+        if (sum >= rand):
+          conn = DbHandler(key)
+          return_list = conn.list_chromosome()
+          conn.close()
+          return return_list
 
 def createGeneration():
+  global Adaptability
   with open('genstate', 'r') as f:
     genstate = f.read()
   # print(genstate)
@@ -51,13 +105,40 @@ def createGeneration():
       conn_new.close()
     conn.close()
   else:
-    pass
+    # 交叉，变异
+    # crossandmutation
+    for i in range(0,crossoverMutationNum):
+      # 采用轮盘赌选择父母染色体
+      newChromosomeMatrix = []
+      chromosomeBaba = RWS()
+      chromosomeMama = RWS()
+      for i in range(0,len(chromosomeBaba)):
+        if random.random()<0.5:
+          newChromosomeMatrix.append(chromosomeBaba[i])
+        else:
+          newChromosomeMatrix.append(chromosomeMama[i])
+      conn_new = DbHandler('{}_{}.db'.format(int(genstate)+1,i))
+      conn_new.Create()
+      for row in newChromosomeMatrix:
+        conn_new.insert(row[0],variation.variation(row[2],row[1]),row[2])
+      conn_new.close()
+    sorted_x=sorted(Adaptability.items(), key=operator.itemgetter(1))
+    print("move")
+    print(Adaptability)
+    print(sorted_x)
+    for i in range(crossoverMutationNum,chromosomeNum):
+      shutil.copy(sorted_x[i][0], '{}_{}.db'.format(int(genstate)+1,i))
   with open('genstate', 'w') as f:
     f.write(str(int(genstate)+1))
 
 def gaSearch():
-  createGeneration()
-
+  for i in range(0,iteratorNum):
+    # 生成新一代染色体
+    createGeneration()
+    # 计算上一代各条染色体的适应度
+    calAdaptability()
+    # 计算各染色体的适应度
+    calSelectionProbability()
  #
  # 初始化遗传算法
  #
