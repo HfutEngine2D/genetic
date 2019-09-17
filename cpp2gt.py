e2f176c4bf92755f4a7ded0738c906238198f638
bcahlit <bcahlit@gmail.com>
2019-09-17 20:18:55 +0800

    修改了cpp2gt文件

--- cpp2gt.py
+++ cpp2gt.py
@@ -1,19 +1,22 @@
-#!/usr/bin/python3
+#!/usr/local/bin/python3
 # -*- coding: utf-8 -*-~
 # 每个都要design point都要起名字太麻烦了，用这个可以随机命名，在cpp里使用_name_作为名称占用符，会按顺序替换成数字
 import re
+
+reSplitExpress = re.compile(r'[\/\.]+')
+design_point = re.compile(r'[\s\S]*(\$[\s\S]*\$)[\s\S]*')
+
 files=['test2.cpp']
 
 case_total_num = 0
 
-def addRandomeName(files):
-  # 用来把字符串中的设计点提取出来，以及替换回去。
-  design_point = re.compile(r'[\s\S]*(\$[\s\S]*\$)[\s\S]*')
+def addRandomeName(file,line):
   # 提取出来的数据
   matchObj = design_point.match(line)
   global case_total_num
   if matchObj:
-    return line.replace('_name_', str(case_total_num))
+    case_total_num += 1
+    return line.replace('_name_', reSplitExpress.split(file)[-2]+str(case_total_num))
   else:
     # 此行代码 没有设计点，直接返回
     return line
@@ -22,7 +25,7 @@ for file in files:
   fr = open("./"+file,'r')
   fw = open(file+'.gt','w')
   for line in fr.readlines():
-    line = addRandomeName(line)
+    line = addRandomeName(file,line)
     print(line)
     fw.write(line)
   fw.close()
